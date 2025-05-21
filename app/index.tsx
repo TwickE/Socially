942f13ae542824f8ec3206c7e6a0820a8929f92a
TwickE <118013263+TwickE@users.noreply.github.com>
2025-05-21 11:18:32 +0100

    Started setting up the project

---
+++ app/index.tsx
@@ -0,0 +1,12 @@
+import { styles } from "@/styles/auth.styles";
+import { Link } from "expo-router";
+import { View } from "react-native";
+
+export default function Index() {
+  return (
+    <View style={styles.container}>
+      <Link href="/notifications">Visit Notifications Screen</Link>
+    </View>
+  );
+}
+
