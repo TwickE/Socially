990457527585817690e146e068ba64000987a330
TwickE <118013263+TwickE@users.noreply.github.com>
2025-05-22 13:32:47 +0100

    Implemented the stories section with some fake data

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,15 +1,35 @@
-import { styles } from "@/styles/auth.styles";
+import Story from "@/components/Story";
+import { stories } from "@/constants/mock-data";
+import { colors } from "@/constants/theme";
+import { styles } from "@/styles/feed.styles";
 import { useAuth } from "@clerk/clerk-expo";
-import { Text, TouchableOpacity, View } from "react-native";
+import { Ionicons } from "@expo/vector-icons";
+import { ScrollView, Text, TouchableOpacity, View } from "react-native";
 
 export default function Index() {
   const { signOut } = useAuth();
 
   return (
     <View style={styles.container}>
-      <TouchableOpacity onPress={() => signOut()}>
-        <Text style={{ color: "white" }}>Sign Out</Text>
-      </TouchableOpacity>
+      {/* HEADER */}
+      <View style={styles.header}>
+        <Text style={styles.headerTitle}>Socially</Text>
+        <TouchableOpacity onPress={() => signOut()}>
+          <Ionicons name="log-out-outline" size={24} color={colors.white} />
+        </TouchableOpacity>
+      </View>
+      {/* STORIES */}
+      <ScrollView showsVerticalScrollIndicator={false}>
+        <ScrollView
+          horizontal
+          showsHorizontalScrollIndicator={false}
+          style={styles.storiesContainer}
+        >
+          {stories.map((story) => (
+            <Story key={story.id} story={story} />
+          ))}
+        </ScrollView>
+      </ScrollView>
     </View>
   );
 }
---
+++ constants/mock-data.ts
@@ -0,0 +1,50 @@
+export const stories = [
+  {
+    id: "1",
+    username: "You",
+    avatar: "https://i.pravatar.cc/300?img=9",
+    hasStory: false,
+  },
+  {
+    id: "2",
+    username: "sarah.k",
+    avatar: "https://i.pravatar.cc/300?img=2",
+    hasStory: true,
+  },
+  {
+    id: "3",
+    username: "alex.m",
+    avatar: "https://i.pravatar.cc/300?img=3",
+    hasStory: true,
+  },
+  {
+    id: "4",
+    username: "julia.r",
+    avatar: "https://i.pravatar.cc/300?img=4",
+    hasStory: true,
+  },
+  {
+    id: "5",
+    username: "mike.rs",
+    avatar: "https://i.pravatar.cc/300?img=5",
+    hasStory: true,
+  },
+  {
+    id: "6",
+    username: "jane.d",
+    avatar: "https://i.pravatar.cc/300?img=10",
+    hasStory: true,
+  },
+  {
+    id: "7",
+    username: "claudia",
+    avatar: "https://i.pravatar.cc/300?img=7",
+    hasStory: true,
+  },
+  {
+    id: "8",
+    username: "mike.rs",
+    avatar: "https://i.pravatar.cc/300?img=8",
+    hasStory: true,
+  }
+];
